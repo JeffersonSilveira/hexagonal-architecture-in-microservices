fcbe9ca84c9147f000edc4c536acdd468c193c08
JeffersonSilveira <jeffersonsilveira30@gmail.com>
2024-03-07 12:42:51 -0300

    basic structure for the project

---
+++ src/main/java/com/project/hexagonal/HexagonalArchitectureInMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.project.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitectureInMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalArchitectureInMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/hexagonal/HexagonalArchitectureInMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureInMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
